645f2ff96b03b85ec97db937af6829a25a5a1416
Gustavo Lopes <guhrlopes999@gmail.com>
2026-01-12 13:57:44 -0300

    refatoração

--- lista-tarefas/script.js
+++ lista-tarefas/script.js
@@ -1,26 +1,29 @@
 //Cria a função adicionarTarefa
 function adicionarTarefa() {
-
-  //Cria uma variável que armazena uma mensagem dentro dela
-  let mensagemSucesso = "Tarefa adicionada com sucesso!";
-
   //a variável inputTarefa, recebe o elemento com id de "inputTarefa" do nosso HTML
-  let inputTarefa = document.getElementById("inputTarefa");
+  const inputTarefa = document.getElementById("inputTarefa");
   //coloca o VALOR do inputTarefa dentro da variável tarefa
   //o trim retira os espaços no começo e no final do texto
   let tarefa = inputTarefa.value.trim();
 
+  const mensagem = document.getElementById("mensagem");
+
   //se o valor do input for vazio então mostre uma mensagem de erro para o usuário
   if(tarefa == "")
   {
+    mensagem.style.color = "#A34743";
     mensagemErro = "Digite uma tarefa para adicioná-la a sua lista!!";
-    document.getElementById("mensagem").textContent = mensagemErro;
+    
+    mensagem.textContent = mensagemErro;
   } else
   {
+    mensagem.style.color = "#28A745";
+    //Cria uma variável que armazena uma mensagem dentro dela
+    let mensagemSucesso = "Tarefa adicionada com sucesso!";
     //pega o elemento com ID "mensagem" no nosso HTML e troca o conteúdo do texto pela variável mensagem que declaramos no começo do código
-    document.getElementById("mensagem").textContent = mensagemSucesso;
+    mensagem.textContent = mensagemSucesso;
     //pega o elemento com o ID "listaTarefas" do nosso HTML (que é uma tag de lista UL)
-    let listaTarefas = document.getElementById("listaTarefas");
+    const listaTarefas = document.getElementById("listaTarefas");
     //cria um novo elemento LI para a nossa tag lista
     let novaTarefa = document.createElement("li");
     //coloca o VALOR do input tarefa dentro desse LI criado
