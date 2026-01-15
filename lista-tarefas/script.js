704176b7f5f734ff58d1633c86b9f9af4a4d2d73
Gustavo Lopes <guhrlopes999@gmail.com>
2026-01-15 10:14:09 -0300

    Projeto Finalizado

--- lista-tarefas/script.js
+++ lista-tarefas/script.js
@@ -32,11 +32,64 @@ function renderizarTarefas()
   const listaTarefas = document.getElementById("listaTarefas");
   listaTarefas.innerHTML = "";
 
-  let i = 0;
-  for (i; i < tarefas.length; i++)
+  for (let i = 0; i < tarefas.length; i++)
   {
     let novaTarefa = document.createElement("li");
     novaTarefa.textContent = tarefas[i];
+
+    let botaoRemover = document.createElement("button");
+    botaoRemover.className = "remover";
+    botaoRemover.textContent = "Remover";
+    botaoRemover.onclick = () => removerTarefa(i);
+
+    let botaoEditar = document.createElement("button");
+    botaoEditar.className = "editar";
+    botaoEditar.textContent = "Editar";
+    botaoEditar.onclick = () => editarTarefa(i);
+
+    novaTarefa.appendChild(botaoRemover);
+    novaTarefa.appendChild(botaoEditar);
     listaTarefas.appendChild(novaTarefa);
   }
-} 
+}
+
+function removerTarefa (i)
+{
+  tarefas.splice(i, 1);
+  mensagem.style.color = "#000000";
+  let mensagemRemover = "Tarefa Removida com sucesso!";
+  mensagem.textContent = mensagemRemover;
+  renderizarTarefas();
+}
+
+function editarTarefa (i)
+{
+  let tarefaEditada = prompt("Edite a tarefa:");
+  if (tarefaEditada.trim() !== "")
+  {
+    tarefas[i] = tarefaEditada;
+    mensagem.style.color = "#000000";
+    let mensagemEditar = "Tarefa Editada com sucesso!";
+    mensagem.textContent = mensagemEditar;
+    renderizarTarefas();
+  }
+}
+
+function limparLista ()
+{
+  if (tarefas.length == 0)
+  {
+    mensagem.style.color = "#000000";
+    let mensagemVazia = "Não existem tarefas para serem limpas!";
+    mensagem.textContent = mensagemVazia;
+    renderizarTarefas();
+  } else
+  {
+    tarefas.length = 0;
+    mensagem.style.color = "#000000";
+    let mensagemLista = "Lista limpa com sucesso!";
+    mensagem.textContent = mensagemLista;
+    renderizarTarefas();
+  }
+  
+}
